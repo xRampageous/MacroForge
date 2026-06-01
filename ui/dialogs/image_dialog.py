9a58089db9849ce0bb43906ce85ff7c722ed3e86
xRampageous <xRampageous@users.noreply.github.com>
2026-06-02 06:47:42 +1000

    v1.2.38: removed QApplication.processEvents and hide/show from _do_capture

--- ui/dialogs/image_dialog.py
+++ ui/dialogs/image_dialog.py
@@ -607,10 +607,6 @@ class ImageDialog(QDialog):
         except ImportError:
             QMessageBox.warning(self, "Missing Dependency", "Screen capture requires Pillow:\npip install pillow")
             return
-        self.hide()
-        QApplication.processEvents()
-        import time
-        time.sleep(0.15)
         overlay = CaptureOverlay()
         loop = QEventLoop()
         overlay.closed.connect(loop.quit)
@@ -620,10 +616,6 @@ class ImageDialog(QDialog):
         logger.debug("image_dialog._do_capture: starting event loop")
         loop.exec()
         logger.debug("image_dialog._do_capture: event loop finished")
-        time.sleep(0.05)
-        self.show()
-        self.raise_()
-        self.activateWindow()
         if overlay.region:
             x, y, w, h = overlay.region
             logger.debug(f"image_dialog._do_capture: region={x},{y},{w},{h}")
